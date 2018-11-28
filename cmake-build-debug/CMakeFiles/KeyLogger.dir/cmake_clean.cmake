fbaad0198e415f93b80247302d3d7a54ea6ab55d
kocurc <kordian.o@wp.pl>
2018-11-29 00:13:40 +0100

    First commit.

---
+++ cmake-build-debug/CMakeFiles/KeyLogger.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KeyLogger.dir/main.cpp.obj"
+  "KeyLogger.pdb"
+  "KeyLogger.exe"
+  "KeyLogger.exe.manifest"
+  "libKeyLogger.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KeyLogger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
